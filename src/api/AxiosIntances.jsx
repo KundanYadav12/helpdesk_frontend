d4d92b8f56acccb03ffaf7040e08701d04870353
KundanYadav12 <157380373+KundanYadav12@users.noreply.github.com>
2025-04-25 08:52:17 +0530

    Update AxiosIntances.jsx

--- src/api/AxiosIntances.jsx
+++ src/api/AxiosIntances.jsx
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://helpdesk-backend-zdet.onrender.com/api',
 });
 
 // Optional: Interceptor to add token
